b03bfc6b3b3b4d8a9d3b0f8d3f1db4d1493d98d7
LearningFlutter373 <ajaykumar.dev25@gmail.com>
2024-10-20 16:56:17 +0530

    i changed colors in home screen

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -7,7 +7,7 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Text("Home Screen",style: TextStyle(fontSize: 30,color: Colors.orange),),
+        child: Text("Home Screen",style: TextStyle(fontSize: 30,color: Colors.red),),
       ),
     );
   }
