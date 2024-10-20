60c77ad0ef8f180ebe834f7d49368573d5a3394b
LearningFlutter373 <ajaykumar.dev25@gmail.com>
2024-10-20 16:59:19 +0530

    i  added a splash screen in this app.

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -5,9 +5,11 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
     return Scaffold(
       body: Center(
         child: Text("Home Screen",style: TextStyle(fontSize: 30,color: Colors.red),),
+
       ),
     );
   }
